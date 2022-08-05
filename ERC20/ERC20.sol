fed6956a78140a8693ad107e4330fccde2b94134
My Ether Authority <43878603+MyEtherAuthority@users.noreply.github.com>
2022-08-05 14:41:44 +0530

    Update ERC20.sol

--- ERC20/ERC20.sol
+++ ERC20/ERC20.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity 0.8.15;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20("TestToken", "TEST") {
+contract Token is ERC20 {
 
-    constructor(uint256 totalSupply){
+    constructor(string memory tokenName, string memory tokenSymbol, uint256 totalSupply) ERC20(tokenName, tokenSymbol){
+
         _mint(msg.sender, totalSupply * (10**decimals()));
     }
 
