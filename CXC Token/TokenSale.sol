4635fc06cbbe7c2459bfc7e30bf60f3bb5bfe629
Amit Kumar Yadav <40794905+imAky10@users.noreply.github.com>
2023-07-03 16:58:12 +0530

    Update TokenSale

--- CXC Token/TokenSale.sol
+++ CXC Token/TokenSale.sol
@@ -2,136 +2,8 @@
 // File: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol
 // OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
 
-pragma solidity 0.7.6;
+pragma solidity 0.8.17;
 
-
-
-library FullMath {
-    /// @notice Calculates floor(a×b÷denominator) with full precision. Throws if result overflows a uint256 or denominator == 0
-    /// @param a The multiplicand
-    /// @param b The multiplier
-    /// @param denominator The divisor
-    /// @return result The 256-bit result
-    /// @dev Credit to Remco Bloemen under MIT license https://xn--2-umb.com/21/muldiv
-    function mulDiv(
-        uint256 a,
-        uint256 b,
-        uint256 denominator
-    ) internal pure returns (uint256 result) {
-        // 512-bit multiply [prod1 prod0] = a * b
-        // Compute the product mod 2**256 and mod 2**256 - 1
-        // then use the Chinese Remainder Theorem to reconstruct
-        // the 512 bit result. The result is stored in two 256
-        // variables such that product = prod1 * 2**256 + prod0
-        uint256 prod0; // Least significant 256 bits of the product
-        uint256 prod1; // Most significant 256 bits of the product
-        assembly {
-            let mm := mulmod(a, b, not(0))
-            prod0 := mul(a, b)
-            prod1 := sub(sub(mm, prod0), lt(mm, prod0))
-        }
-
-        // Handle non-overflow cases, 256 by 256 division
-        if (prod1 == 0) {
-            require(denominator > 0);
-            assembly {
-                result := div(prod0, denominator)
-            }
-            return result;
-        }
-
-        // Make sure the result is less than 2**256.
-        // Also prevents denominator == 0
-        require(denominator > prod1);
-
-        ///////////////////////////////////////////////
-        // 512 by 256 division.
-        ///////////////////////////////////////////////
-
-        // Make division exact by subtracting the remainder from [prod1 prod0]
-        // Compute remainder using mulmod
-        uint256 remainder;
-        assembly {
-            remainder := mulmod(a, b, denominator)
-        }
-        // Subtract 256 bit number from 512 bit number
-        assembly {
-            prod1 := sub(prod1, gt(remainder, prod0))
-            prod0 := sub(prod0, remainder)
-        }
-
-        // Factor powers of two out of denominator
-        // Compute largest power of two divisor of denominator.
-        // Always >= 1.
-        uint256 twos = -denominator & denominator;
-        // Divide denominator by power of two
-        assembly {
-            denominator := div(denominator, twos)
-        }
-
-        // Divide [prod1 prod0] by the factors of two
-        assembly {
-            prod0 := div(prod0, twos)
-        }
-        // Shift in bits from prod1 into prod0. For this we need
-        // to flip `twos` such that it is 2**256 / twos.
-        // If twos is zero, then it becomes one
-        assembly {
-            twos := add(div(sub(0, twos), twos), 1)
-        }
-        prod0 |= prod1 * twos;
-
-        // Invert denominator mod 2**256
-        // Now that denominator is an odd number, it has an inverse
-        // modulo 2**256 such that denominator * inv = 1 mod 2**256.
-        // Compute the inverse by starting with a seed that is correct
-        // correct for four bits. That is, denominator * inv = 1 mod 2**4
-        uint256 inv = (3 * denominator) ^ 2;
-        // Now use Newton-Raphson iteration to improve the precision.
-        // Thanks to Hensel's lifting lemma, this also works in modular
-        // arithmetic, doubling the correct bits in each step.
-        inv *= 2 - denominator * inv; // inverse mod 2**8
-        inv *= 2 - denominator * inv; // inverse mod 2**16
-        inv *= 2 - denominator * inv; // inverse mod 2**32
-        inv *= 2 - denominator * inv; // inverse mod 2**64
-        inv *= 2 - denominator * inv; // inverse mod 2**128
-        inv *= 2 - denominator * inv; // inverse mod 2**256
-
-        // Because the division is now exact we can divide by multiplying
-        // with the modular inverse of denominator. This will give us the
-        // correct result modulo 2**256. Since the precoditions guarantee
-        // that the outcome is less than 2**256, this is the final result.
-        // We don't need to compute the high bits of the result and prod1
-        // is no longer required.
-        result = prod0 * inv;
-        return result;
-    }
-
-    /// @notice Calculates ceil(a×b÷denominator) with full precision. Throws if result overflows a uint256 or denominator == 0
-    /// @param a The multiplicand
-    /// @param b The multiplier
-    /// @param denominator The divisor
-    /// @return result The 256-bit result
-    function mulDivRoundingUp(
-        uint256 a,
-        uint256 b,
-        uint256 denominator
-    ) internal pure returns (uint256 result) {
-        result = mulDiv(a, b, denominator);
-        if (mulmod(a, b, denominator) > 0) {
-            require(result < type(uint256).max);
-            result++;
-        }
-    }
-}
-
-/// @title FixedPoint96
-/// @notice A library for handling binary fixed point numbers, see https://en.wikipedia.org/wiki/Q_(number_format)
-/// @dev Used in SqrtPriceMath.sol
-library FixedPoint96 {
-    uint8 internal constant RESOLUTION = 96;
-    uint256 internal constant Q96 = 0x1000000000000000000000000;
-}
 
 // IERC20 standard interface
 interface IERC20
@@ -146,18 +18,11 @@ interface IERC20
 //USDT contract in Ethereum does not follow ERC20 standard so it needs different interface
 interface IERC20_USDT
 {
-    function transferFrom(address _from, address _to, uint256 _amount) external;
+    function transfer(address _to, uint256 _amount) external;
+    function transferFrom(address _from, address _to, uint _value) external;
+    function balanceOf(address who) external returns (uint);
 }
 
-
-// Interface for Uniswap V3 Pool contract
-interface Pool
-{
-    function token0() external view returns(address);
-    function token1() external view returns(address);
-    function liquidity() external view returns(uint168);
-    function slot0() external view returns( uint160, int24,  uint16,  uint16,  uint16,  uint8,  bool);
-}
 
 
 // Ownership smart contract
@@ -222,6 +87,7 @@ contract TokenSale is Ownable{
     //public variables
     uint256 public tokensSold;
     IERC20 public token;
+    address public usdtToken;
 
     uint256 public exchangeRateInEth;
     uint256 public exchangeRateInUSDT;
@@ -235,10 +101,12 @@ contract TokenSale is Ownable{
 
     constructor(
         IERC20 _token,
+        address _usdtToken,
         uint256 _exchangeRateInEth,
         uint256 _exchangeRateInUSDT
     ) {
         token = _token;
+        usdtToken = _usdtToken;
         exchangeRateInEth = _exchangeRateInEth;
         exchangeRateInUSDT = _exchangeRateInUSDT;
     }
@@ -264,7 +132,6 @@ contract TokenSale is Ownable{
     */
     function buyTokens(uint256 tokenAmountinUSDT) external payable {
         uint256 amount;
-        address token0 = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
         if(msg.value > 0){
             require (msg.value <= maxAmountinEth, "Cannot buy more than max limit");
             /* spend token is ETHER*/
@@ -272,14 +139,15 @@ contract TokenSale is Ownable{
             amount = msg.value * exchangeRateInEth;
             token.transfer(msg.sender, amount);
             payable(owner()).transfer(msg.value);
-        
-        }else{
+        }
+        else{
+            require(IERC20_USDT(usdtToken).balanceOf(msg.sender) >= tokenAmountinUSDT, "Not sufficient balance");
             require(tokenAmountinUSDT > 0, "Token amount should be greater than zero");
             require(tokenAmountinUSDT <= maxAmountinUSDT, "Cannot buy more than max limit");
 
             amount = tokenAmountinUSDT * exchangeRateInUSDT;
            
-            IERC20_USDT(token0).transferFrom(msg.sender, owner(), tokenAmountinUSDT);
+            IERC20_USDT(usdtToken).transferFrom(msg.sender, owner(), tokenAmountinUSDT);
             
             token.transfer(msg.sender, amount);
 
@@ -287,11 +155,9 @@ contract TokenSale is Ownable{
 
         tokensSold += amount;
 
-        emit TokensPurchased(msg.sender, token0, amount);
-
+        emit TokensPurchased(msg.sender, usdtToken, amount);
         
     }
-
 
     /**
     * This lets owner to withdraw any leftover tokens.
