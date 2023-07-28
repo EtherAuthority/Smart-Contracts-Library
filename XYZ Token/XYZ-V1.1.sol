f696a1abdde07c3bab7af75f6ee595cf29239e17
Amit Kumar Yadav <40794905+imAky10@users.noreply.github.com>
2023-07-28 18:43:28 +0530

    Update XYZ-V1.1.sol

--- XYZ Token/XYZ-V1.1.sol
+++ XYZ Token/XYZ-V1.1.sol
@@ -220,7 +220,6 @@ contract XYZToken is Ownable {
     IUniswapV2Router02 public uniswapV2Router;
     address public _uniswapPair;
 
-    uint256 public _taxCollected;
     bool public _inSwapAndLiquify = false;
 
     //events
@@ -486,15 +485,15 @@ contract XYZToken is Ownable {
 
     //anyone can call this function. this is by design.
     function liquifyTokens() external{
-         if (_taxCollected >= _taxThreshold ) {
-            _taxCollected = 0;
+         if (balanceOf(address(this)) >= _taxThreshold ) {
             swapAndLiquify();
         }
        
     }
 
-    function rescueETH() external onlyOwner {
+    function withdrawTokens() external onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
+        _transferTokens(address(this), msg.sender, balanceOf(address(this)));
     }
 
     function swapTokensForEth(uint256 tokenAmount) private {
@@ -624,8 +623,6 @@ contract XYZToken is Ownable {
 
         taxAmount = buyTax + sellTax;
         _transferTokens(sender, address(this), taxAmount); // send buy tax and sell tax to contract
-
-        _taxCollected += buyTax + sellTax + liquidityTax;
 
         uint256 transferAmount = amount - (buyTax) - (sellTax) - (liquidityTax) - (burnTax);
         _transferTokens(sender, recipient, transferAmount); // send to recipient        
