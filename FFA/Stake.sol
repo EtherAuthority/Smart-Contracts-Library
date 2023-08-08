7817bc056c59ee0cb31555f832eb0569dd4c505e
Jinal Joshi <joshi.jinal19@gmail.com>
2023-08-08 22:22:59 +0530

    Update Stake.sol
    new updates

--- FFA/Stake.sol
+++ FFA/Stake.sol
@@ -15,13 +15,13 @@ contract Stake {
 
     //_stakigntime=month; _stakedtime=on which block u staked;
     struct _staking{
-        uint _stakingtime;
-        uint _stakedtime;
+        uint _stakingStarttime;
+        uint _stakingEndtime;
         uint _amount;
         uint _profit;
     }
 
-    mapping(address=>_staking) public staking;    
+    mapping(address=>mapping(uint256=>_staking)) public staking;    
     mapping(uint => uint) public RewardPercentage;
     address public RewardPoolAddress;
     address public tokenAddress=address(0);
@@ -40,7 +40,7 @@ contract Stake {
         RewardPercentage[180] = 3500;
         RewardPercentage[360] = 160000;
 
-       
+        
     }  
 
     modifier onlyOwner() {
@@ -51,20 +51,20 @@ contract Stake {
    
   }
 
-    event withdrawprofit(address _to, uint _amount);
+   
     event unstake(address _to, uint _amount);
 
     function changeRewardPoolAddress( address _rewardaddress) public {
         RewardPoolAddress = _rewardaddress;
     }   
 
-    function RewardPercentageChange( uint _month , uint _percentage) public returns(uint) {
-        RewardPercentage[_month] = _percentage;
-        return  RewardPercentage[_month];
+    function RewardPercentageChange( uint _stakeDays , uint _percentage) public returns(uint) {
+        RewardPercentage[_stakeDays] = _percentage;
+        return  RewardPercentage[_stakeDays];
     }
 
-    function viewPercentage(uint _month) public view returns(uint){
-        return RewardPercentage[_month];
+    function viewPercentage(uint _stakeDays) public view returns(uint){
+        return RewardPercentage[_stakeDays];
     }
 
     function setRewardToken(uint _amount) public {
@@ -72,78 +72,55 @@ contract Stake {
        
     }   
 
-    // function calculateprofit() internal returns(uint){
-    //     require(staking[msg.sender]._amount > 0,"Wallet Address is not Exist");
-    //     uint month=staking[msg.sender]._stakingtime;
-    //     uint profit = staking[msg.sender]._amount *  RewardPercentage[month]/100;
-    //     //staking[msg.sender]._profit = profit;
-    //     return profit;
-    // }
+   
 
-    function viewProfit() public view returns(uint){
-        require(staking[msg.sender]._amount > 0,"Wallet Address is not Exist");
-        uint profit = staking[msg.sender]._amount *  RewardPercentage[staking[msg.sender]._stakingtime]/10000;
+    function viewProfit(uint256 _stakeDays ) public view returns(uint){
+        require(staking[msg.sender][_stakeDays]._amount > 0,"Wallet Address is not Exist");
+        uint profit = staking[msg.sender][_stakeDays]._amount *  RewardPercentage[_stakeDays]/10000;
         return profit;
     }
 
     function stake(uint _staketime , uint _stakeamount) public returns (bool){
-        require(staking[msg.sender]._amount == 0,"Wallet Address is already Exist");
+        require(staking[msg.sender][_staketime]._amount == 0,"Wallet Address is already Exist");
         require(TokenI(tokenAddress).balanceOf(msg.sender) > _stakeamount, "Insufficient tokens");
+        require(RewardPercentage[_staketime] > 0,"Please enter valid stack days");
         uint profit = _stakeamount * RewardPercentage[_staketime]/10000;
-        staking[msg.sender] =  _staking(_staketime,block.timestamp,_stakeamount,profit);
+        staking[msg.sender][_staketime] =  _staking(block.timestamp,block.timestamp + (_staketime*(24*60*60)),_stakeamount,profit);
 
         TokenI(tokenAddress).transfer(address(this), _stakeamount);
         return true;
+       
     }
 
-    function withdrawProfit() public returns (bool){            
+
+ function unStake(uint256 _stakeDays) public returns (bool){            
             
-             //uint locktime=staking[msg.sender]._stakedtime+(30 * staking[msg.sender]._stakingtime *(24*60*60));
-             uint locktime=staking[msg.sender]._stakedtime+600;             
-            require(staking[msg.sender]._amount > 0,"Wallet Address is not Exist");
-            require(block.timestamp > locktime,"Tokens are Locked");
-            uint profit= staking[msg.sender]._profit;
-            
-            staking[msg.sender]._profit=0;
-                 
-
-            TokenI(tokenAddress).transfer(msg.sender, profit);
-
-            
-             emit withdrawprofit(msg.sender,profit);
-            return true;
-
-             
-           
-    }
-
- function unStake() public returns (bool){            
-             //uint locktime=deployTimestamp + (30 * stakeTime[msg.sender] *(24*60*60));
-            // uint locktime=staking[msg.sender]._stakedtime+(30 * staking[msg.sender]._stakingtime *(24*60*60));
-             uint locktime=staking[msg.sender]._stakedtime+600;
+            require(staking[msg.sender][_stakeDays]._amount > 0,"Wallet Address is not Exist");
+            require(RewardPercentage[_stakeDays] > 0,"Please enter valid stack days");
+            uint locktime=staking[msg.sender][_stakeDays]._stakingStarttime+600;
             
              
-            require(staking[msg.sender]._amount > 0,"Wallet Address is not Exist");
+            
             uint totalAmt;
             uint profit;
             uint remainingProfit;
 
             if(block.timestamp > locktime){
             
-                profit= staking[msg.sender]._profit;
-                totalAmt= staking[msg.sender]._amount+ profit;
+                profit= staking[msg.sender][_stakeDays]._profit;
+                totalAmt= staking[msg.sender][_stakeDays]._amount+ profit;
 
             }else{
 
-                profit= staking[msg.sender]._profit;
+                profit= staking[msg.sender][_stakeDays]._profit;
                 remainingProfit=profit/2; //penalty
-                totalAmt= staking[msg.sender]._amount+ remainingProfit;
+                totalAmt= staking[msg.sender][_stakeDays]._amount+ remainingProfit;
 
             }
-            staking[msg.sender]._amount=0;
-            staking[msg.sender]._stakedtime=0;
-            staking[msg.sender]._stakingtime=0;
-            staking[msg.sender]._profit=0;
+            staking[msg.sender][_stakeDays]._amount=0;
+            staking[msg.sender][_stakeDays]._stakingStarttime=0;
+            staking[msg.sender][_stakeDays]._stakingEndtime=0;
+            staking[msg.sender][_stakeDays]._profit=0;
                  
 
             TokenI(tokenAddress).transfer(msg.sender, totalAmt);
