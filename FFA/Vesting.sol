2373e131456367f2678e22e94cbcb3c60d3ef89a
Jinal Joshi <joshi.jinal19@gmail.com>
2023-08-15 20:26:05 +0530

    Update Vesting.sol

--- FFA/Vesting.sol
+++ FFA/Vesting.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.19;
 
-import "./DateTime.sol";
-
-
-// ERC20 Token contract interface
-
+/**
+* @dev  ERC20 Token contract interface
+*/
 interface Token {
     
     function transfer(address to, uint256 amount) external returns(bool);
@@ -35,92 +32,77 @@ contract Vesting {
     uint public onemonth = 60;
     uint256 public decimals;
    
-     function getYear(uint _timeStemp) internal  pure returns (uint256 year) {
-        year = DateTime.getYear(_timeStemp);
-    }
-
-    // Years Ends with fab and 1st march we can withdraw maturity amount  
-    function timestampFromDateTime(uint _timeStemp)
-        internal
-        pure
-        returns (uint256 timestamp)
-    {
-        uint year=getYear(_timeStemp);
-        return DateTime.timestampFromDateTime(year, 0 , 1, 0, 0, 0);
-    }
     
     constructor( address _tokenContract) {
-
         owner=msg.sender;
         tokenContract= _tokenContract; 
         //deployTimestamp = timestampFromDateTime(block.timestamp);
         deployTimestamp = block.timestamp;          
         decimals=Token(tokenContract).decimals();
 
-       
-
          // Category A
          lockingWallet[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2]=37500000000* (10**decimals);// Team Allocation Assigned Tokens 
          VestingTime[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2]=24; //lock months
-         //unlockDate["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"] =  timestampFromDateTime(deployTimestamp + (31*_vestingTime[i]*(24*60*60)));// unlock start
+         //unlockDate["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"] =  deployTimestamp + (31*_vestingTime[i]*(24*60*60));// unlock start
          unlockDate[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] =  deployTimestamp + (120);
             /*
          // Category B
          lockingWallet[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2]=37500000000* (10**decimals);// Team Allocation Assigned Tokens 
          VestingTime[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2]=24; //lock months
-         //unlockDate["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"] =  timestampFromDateTime(deployTimestamp + (31*_vestingTime[i]*(24*60*60)));// unlock start
+         //unlockDate["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"] =  deployTimestamp + (31*_vestingTime[i]*(24*60*60));// unlock start
          unlockDate[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] =  deployTimestamp + (120);
 
          // Category c
          lockingWallet[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2]=37500000000* (10**decimals);// Team Allocation Assigned Tokens 
          VestingTime[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2]=24; //lock months
-         //unlockDate["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"] =  timestampFromDateTime(deployTimestamp + (31*_vestingTime[i]*(24*60*60)));// unlock start
+         //unlockDate["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"] =  deployTimestamp + (31*_vestingTime[i]*(24*60*60));// unlock start
          unlockDate[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] =  deployTimestamp + (120);
 
          // Category D
          lockingWallet[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2]=37500000000* (10**decimals);// Team Allocation Assigned Tokens 
          VestingTime[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2]=24; //lock months
-         //unlockDate["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"] =  timestampFromDateTime(deployTimestamp + (31*_vestingTime[i]*(24*60*60)));// unlock start
+         //unlockDate["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"] =  deployTimestamp + (31*_vestingTime[i]*(24*60*60));// unlock start
          unlockDate[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] =  deployTimestamp + (120);
 
             */
     } 
- 
 
+    /**
+    * @dev To show contract event  .
+    */
     event withdraw(address _to, uint _amount);
 
-      /**
-     * @dev ViewVestingAmount shows available Team Allocation amount of particular wallet
-     * parameters : user (wallet Address)
-     *              
-     */
-   
-   function ViewVestingAmount( address user )public view returns (uint){ 
-        uint tempVer = 0; 
-             for(uint i=0;i<12;i++) 
-             { 
-                 require(unlockDate[user]+onemonth<=block.timestamp,"Unable to Withdraw"); 
+    /**
+    * @dev ViewVestingAmount shows available Team Allocation amount of particular wallet
+    * parameters : user (wallet Address)
+    *              
+    */   
+    function ViewVestingAmount( address user )public view returns (uint){ 
+        uint VestingAmount = 0; 
+        for(uint i=0;i<12;i++) 
+        { 
+            if(unlockDate[user]+onemonth<=block.timestamp){
                  if(block.timestamp>=unlockDate[user]+(onemonth*i)) 
                  { 
                      if(withdrawdetails[user][i].time==0) 
                      { 
-                        tempVer+=lockingWallet[user]/12;                        
+                        VestingAmount+=lockingWallet[user]/12;                        
                      } 
                  }                                                                                                                                                                                                                          
                  else 
                  { 
                      break; 
                  } 
-             } 
-             return tempVer; 
+             }
+        }  
+             return VestingAmount; 
     }
-      /**
+    /**
      * @dev Team Allocation amount release in particular category wallet.
      *
      */
-    
-     function withdrawTokens()public returns (bool){ 
-        uint tempVer = 0; 
+    function withdrawTokens()public returns (bool){ 
+        uint VestingAmount = 0; 
              for(uint i=0;i<12;i++) 
              { 
                  require(unlockDate[msg.sender]+onemonth<=block.timestamp,"Unable to Withdraw"); 
@@ -128,7 +110,7 @@ contract Vesting {
                  { 
                      if(withdrawdetails[msg.sender][i+1].time==0) 
                      { 
-                        tempVer+=lockingWallet[msg.sender]/12; 
+                        VestingAmount+=lockingWallet[msg.sender]/12; 
                         withdrawdetails[msg.sender][i+1]=_withdrawdetails(block.timestamp,lockingWallet[msg.sender]/12);                       
                      } 
                  }                                                                                                                                                                                                                          
@@ -137,13 +119,8 @@ contract Vesting {
                      break; 
                  } 
              } 
-             Token(tokenContract).transfer(msg.sender, tempVer);
-            
-             emit withdraw(msg.sender,tempVer);
+             Token(tokenContract).transfer(msg.sender, VestingAmount);            
+             emit withdraw(msg.sender,VestingAmount);
              return true;
     }
-    
- 
-
-    
 }
