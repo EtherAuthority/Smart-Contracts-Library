513e296a228badc1f31cd458b533d53e5f0fe8e0
nigamashwin <77826921+nigamashwin@users.noreply.github.com>
2023-10-11 19:10:45 +0530

    Update solarToken.sol

--- Solar Funding/solarToken.sol
+++ Solar Funding/solarToken.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SolarPowerToken is ERC20 {
+    address public votingContractAddress;
+    uint public maxSupply = 75000000 * (10 ** 18);
+
+    constructor()
+        ERC20("Solar Power Token", "SPT")
+    {}
+
+    function mint(address to, uint256 amount) public {
+        require(msg.sender == votingContractAddress, "only voting contract can call");
+        require(totalSupply() + amount <= maxSupply, "Cannot Mint more than maximum supply");
+        _mint(to, amount);
+    }
+}
