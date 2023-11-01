6639687246f824a5cb004307459723107e5cb13a
nigamashwin <77826921+nigamashwin@users.noreply.github.com>
2023-11-01 13:56:59 +0530

    Update voting.sol

--- Solar Funding/voting.sol
+++ Solar Funding/voting.sol
@@ -20,44 +20,95 @@ interface contractInterface {
     function mint(address to, uint256 amount) external returns (bool);
 }
 
-/// @title Owned Contract
-/// @dev This contract provides ownership management functionality.
-contract owned {
-    address public owner;
-    address public newOwner;
+// File: @openzeppelin/contracts/access/Ownable.sol
 
-    event OwnershipTransferred(address indexed _from, address indexed _to);
 
-    /// @dev Initializes the owner as the deployer of the contract.
+// OpenZeppelin Contracts (last updated v4.9.0) (access/Ownable.sol)
+
+
+
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
     constructor() {
-        owner = msg.sender;
+        _transferOwnership(_msgSender());
     }
 
-    /// @dev Modifier that allows only the owner to execute a function.
-    modifier onlyOwner {
-        require(msg.sender == owner, "Only the owner can call this function");
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
         _;
     }
 
-    /// @notice Transfer ownership to a new address.
-    /// @param _newOwner The address to transfer ownership to.
-    function transferOwnership(address _newOwner) public onlyOwner {
-        newOwner = _newOwner;
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
     }
 
-    /// @notice Accept the pending ownership transfer.
-    function acceptOwnership() public {
-        require(msg.sender == newOwner, "Only the new owner can accept ownership");
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-        newOwner = address(0);
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby disabling any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
     }
 }
 
 /// @title VotingForSolar
 /// @dev A smart contract for voting on solar-related proposals, with ownership management.
 
-contract VotingForSolar is owned {
+contract VotingForSolar is Ownable {
     mapping(address => bool) public founder;
     uint public totalFounders;
 
