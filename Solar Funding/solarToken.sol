026c28e735acd003b023d5f30ace08194abb5811
SolidityX <88942434+solidityx@users.noreply.github.com>
2023-11-12 11:26:03 +0530

    added admin function to change voting contract in case of emergency. renounce it if not needed

--- Solar Funding/solarToken.sol
+++ Solar Funding/solarToken.sol
@@ -196,6 +196,88 @@ abstract contract Context {
 // OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/IERC20.sol)
 
 
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+ 
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+ 
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+ 
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+ 
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+ 
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+ 
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+ 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(
+            newOwner != address(0),
+            "Ownable: new owner is the zero address"
+        );
+        _transferOwnership(newOwner);
+    }
+ 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
 
 /**
  * @dev Interface of the ERC20 standard as defined in the EIP.
@@ -624,19 +706,34 @@ abstract contract ERC20 is Context, IERC20, IERC20Metadata, IERC20Errors {
 
 
 
-contract SolarRevenueToken is ERC20 {
+contract SolarRevenueToken is ERC20, Ownable {
     address public votingContractAddress;
     uint public maxSupply = 75000000 * (10 ** 18);
+    event VotingContractUpdated(address oldVotingContract, address newVotingContract);
 
     constructor(address _votingContractAddress)
         ERC20("Solar Revenue Token", "SRT")
 
     { votingContractAddress = _votingContractAddress; }
 
+    /**
+        @dev mint function mints new tokens. It is triggered by the voting contract. 
+        @notice It never mints more than max supply.
+    */
     function mint(address to, uint256 amount) public returns(bool){
         require(msg.sender == votingContractAddress, "only voting contract can call");
         require(totalSupply() + amount <= maxSupply, "Cannot Mint more than maximum supply");
         _mint(to, amount);
         return true;
     }
+
+    /**
+        @dev this function is to change the voting contract.
+    */
+    function changeVotingContract(address _votingContractAddress) external onlyOwner {
+        require(_votingContractAddress != address(0), "Invalid address");
+        emit VotingContractUpdated(votingContractAddress, _votingContractAddress);
+        votingContractAddress = _votingContractAddress;
+    }
+
 }
