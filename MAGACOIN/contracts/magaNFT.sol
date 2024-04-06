27639d6185838d6db7d5af099ffe753deadfd2e7
Manish <manishkmrvrm@gmail.com>
2024-04-06 18:32:01 +0530

    update

--- MAGACOIN/contracts/magaNFT.sol
+++ MAGACOIN/contracts/magaNFT.sol
@@ -1575,11 +1575,11 @@ contract MagaNFT is ERC721,Ownable {
      * @notice This function uses block.timestamp, msg.sender, and block.number as inputs
      *         to generate a pseudorandom number within the range of minMonths and maxMonths.
      */
-    function random() private returns (uint) {
-        uint minMonths = 7;
-        uint maxMonths = 11;
-        uint range = (maxMonths - minMonths) + 1;
-        uint months = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, block.number))) % range;
+    function random() private view returns (uint256) {
+        uint256 minMonths = 7;
+        uint256 maxMonths = 11;
+        uint256 range = (maxMonths - minMonths) + 1;
+        uint256 months = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, block.number))) % range;
         months = months + minMonths;
         return months;
     }
@@ -1606,7 +1606,7 @@ contract MagaNFT is ERC721,Ownable {
      * - Marks the MagaCoin tokens as claimed for the specified NFT ID.
      * - Emits a ClaimCoin event.
      */
-    function claimMagaCoin(uint _nftId)external {
+    function claimMagaCoin(uint256 _nftId)external {
         require(magaCoinClaimTime[_nftId] <= block.timestamp,"You can not claim now please wait more");
         require(!coinMinted[_nftId],"Magacoin already claim for this NFT Id");
         
