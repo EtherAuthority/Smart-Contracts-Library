114979e01ddd58f578d1f168408efe30bd8306bf
manish7534 <manishkmrvrm@gmail.com>
2024-04-27 02:16:10 +0530

    update

--- EVOLVEMULTISIGNWALLET/evolveMultisignwallet.sol
+++ EVOLVEMULTISIGNWALLET/evolveMultisignwallet.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17; 
 
-interface AnotherContract {
-    function ownerFunction() external ;
+interface Ivalidator {
+    function renounceOwnership() external;
+    function transferOwnership(address newOwner) external ;
+    function updateGasSettings(uint _validatorPartPercent, uint _burnPartPercent,
+        uint _burnStopAmount, uint _coinPoolPercent,
+        uint _ownerPoolPercent, uint _foundationPercent) external;
+
+    function updateParams(address _foundationWallet, address _ownerPool, address _coinPool,
+        uint256 _ownerPoolColLimit, uint16 _MaxValidators, uint256 _MinimalStakingCoin,
+        uint256 _minimumValidatorStaking) external;
 }
 
 contract MultiSignWallet{
@@ -12,7 +20,7 @@ contract MultiSignWallet{
     address[] public owners;
     mapping(address=>bool) public isOwner;
 
-    AnotherContract public otherContract;
+    Ivalidator public validatorContract;
 
     uint256 public WalletRequired;
     Transaction[] public transactions;
@@ -20,20 +28,39 @@ contract MultiSignWallet{
 
     struct Transaction{
         bool  isExecuted;
+        uint functionTracker;
     }
 
-    /*
-    Constructor function to initialize the MultiSigWallet contract with a list of owners,
-    the required number of owner wallets for transaction execution, and the address of
-    another contract to interact with.
-    Parameters:
-    - _owners: An array of addresses representing the owners of the MultiSigWallet.
-    - _requiredWallet: The minimum number of owner wallets required to execute a transaction.
-    - otherContractAddress: The address of another contract with owner functions that can
-      be executed by this MultiSigWallet.
-    It validates the inputs, initializes the owners, sets the required number of owner wallets,
-    and initializes the interface with the specified address for interacting with the other contract.
-    */
+    struct UpdateGasSetting{
+        uint validatorPer;
+        uint burnPartPer;
+        uint burnStopPer;
+        uint coinPoolPer;
+        uint ownerPoolPer;
+        uint foundationPer;
+    }
+    UpdateGasSetting[] public updateGasSettingInfo;
+
+    struct TransferOwnerTo{
+        address to;
+        bool  isExecuted;
+        string txType;
+    }
+
+    TransferOwnerTo[] public transferOwner;
+
+    struct UpdateParam{
+       address foundationWal;
+       address ownPool;
+       address cPool;
+       uint256 ownerPoolCoLimit;
+       uint16 maxvaldt;
+       uint256 minStakCoin;
+       uint256 minValStk;
+    }
+   
+   UpdateParam[] public paramsInfo;
+
     constructor(address[] memory _owners,uint256 _requiredWallet,address otherContractAddress){
         require(_owners.length>0,"owner required");
         require(_requiredWallet>0 && _requiredWallet<=_owners.length,"invalid required number of owner wallets");
@@ -49,13 +76,16 @@ contract MultiSignWallet{
         }
 
         WalletRequired =_requiredWallet; // you need at least this number wallet to execute transaction
-        otherContract = AnotherContract(otherContractAddress);
+        validatorContract = Ivalidator(otherContractAddress);
     }
 
 
     //-----------------------EVENTS-------------------
 
-    event assignTrnx(uint256 trnx);
+    event AssignRenounceOwnershipTx(uint256 trnx);
+    event AssignTransferOwnershipTx(uint256 trnx);
+    event AssignUpdateGasTx(uint256 trnx);
+    event AssignparamUpdateTx(uint256 trnx);
     event Approve(address owner, uint256 trnxId);
     event Revoke(address owner, uint256 trnxId);
     event Execute(uint256 trnxId);
@@ -83,31 +113,72 @@ contract MultiSignWallet{
         _;
     }
 
-    /*
-    Adds a new transaction to the MultiSigWallet.
-    Each transaction is represented by a Transaction struct,
-    initialized with the flag isExecuted set to false.
-    This function can only be called by the owners of the wallet.
-    Returns: The ID of the newly created transaction.
-    */
-    function newTransaction() external onlyOwner returns(uint256){
-        transactions.push(Transaction({
-            isExecuted:false
-        }));
+    function transferOwnership(address _to) external onlyOwner returns(uint256){
+       transferOwner.push(TransferOwnerTo({
+        to:_to,
+        isExecuted:false,
+        txType:"transferOwnership"
 
-        emit assignTrnx(transactions.length-1);
+       }));
+
+
+        transactions.push(Transaction({
+        isExecuted:false,
+        functionTracker:4
+        }));
+        emit AssignTransferOwnershipTx(transactions.length-1);
         return transactions.length-1;
     }
 
-    /*
-    Allows an owner to approve a transaction identified by its ID.
-    The transaction must exist, must not have been previously approved,
-    and must not have been executed.
-    If the approval count reaches the required number of approvals,
-    the transaction is automatically executed.
-    Parameters:
-    - _trnxId: The ID of the transaction to approve.
-    */
+    function renounceOwnership() external onlyOwner returns(uint) {
+        emit AssignUpdateGasTx(transactions.length-1);
+        transactions.push(Transaction({
+        isExecuted:false,
+        functionTracker:3
+        }));
+        emit AssignRenounceOwnershipTx(transactions.length-1);
+        return transactions.length-1;
+    }
+
+    function updateGasSettings(uint _validatorPer,uint _burnPartPer,uint _burnStopPer,uint _coinPoolPer,uint _ownerPoolPer,uint _foundationPer) external onlyOwner returns(uint256){
+        updateGasSettingInfo.push(UpdateGasSetting({
+            validatorPer: _validatorPer,
+            burnPartPer:_burnPartPer,
+            burnStopPer:_burnStopPer,
+            coinPoolPer:_coinPoolPer,
+            ownerPoolPer:_ownerPoolPer,
+            foundationPer:_foundationPer
+        }));
+         
+        transactions.push(Transaction({
+        isExecuted:false,
+        functionTracker:1
+        }));
+        emit AssignUpdateGasTx(transactions.length-1);
+        return transactions.length-1;
+    }
+
+    function updateParams(address _foundationWal,address _ownPool,address _cPool,uint _ownerPoolCoLimit,uint16 _maxvaldt, uint256 _minStakCoin, uint256 _minValStk) external onlyOwner returns(uint256){
+        paramsInfo.push(UpdateParam({
+            foundationWal:_foundationWal,
+            ownPool:_ownPool,
+            cPool:_cPool,
+            ownerPoolCoLimit:_ownerPoolCoLimit,
+            maxvaldt:_maxvaldt,
+            minStakCoin:_minStakCoin,
+            minValStk:_minValStk
+        }));
+
+
+        transactions.push(Transaction({
+        isExecuted:false,
+        functionTracker:2
+        }));
+        emit AssignparamUpdateTx(transactions.length-1);
+        return transactions.length-1;
+    }
+
+
     function approveTransaction(uint256 _trnxId)
      external onlyOwner
      trnxExists(_trnxId)
@@ -138,30 +209,41 @@ contract MultiSignWallet{
      
     }
 
-    /*
-    Executes a transaction identified by its ID.
-    The transaction must exist and must not have been previously executed.
-    If the required number of approvals is met, the function calls the ownerFunction
-    of the other contract and marks the transaction as executed.
-    Parameters:
-    - _trnxId: The ID of the transaction to execute.
-    */
+
     function executeTransaction(uint256 _trnxId) internal trnxExists(_trnxId) notExecuted(_trnxId){
         require(_getAprrovalCount(_trnxId)>=WalletRequired,"you don't have sufficient approval");
         Transaction storage _transactions = transactions[_trnxId];
-        otherContract.ownerFunction();
-        _transactions.isExecuted = true;
+
+        if(_transactions.functionTracker == 1)
+        {
+                UpdateGasSetting  storage _updateGasSettings = updateGasSettingInfo[updateGasSettingInfo.length-1];
+                validatorContract.updateGasSettings(_updateGasSettings.validatorPer,_updateGasSettings.burnPartPer,
+                _updateGasSettings.burnStopPer, _updateGasSettings.coinPoolPer,
+                _updateGasSettings.ownerPoolPer, _updateGasSettings.foundationPer);
+                
+        }
+        else if(_transactions.functionTracker == 2){
+                UpdateParam  storage _updateParams = paramsInfo[paramsInfo.length-1];
+                validatorContract.updateParams(_updateParams.foundationWal,_updateParams.ownPool, _updateParams.cPool,
+                _updateParams.ownerPoolCoLimit, _updateParams.maxvaldt, _updateParams.minStakCoin,
+                _updateParams.minValStk);
+                
+        }
+         else if(_transactions.functionTracker == 3){
+                validatorContract.renounceOwnership();
+                
+        }
+         else if(_transactions.functionTracker == 4){
+                TransferOwnerTo storage _transferOwnership = transferOwner[transferOwner.length-1];
+                validatorContract.transferOwnership(_transferOwnership.to);
+                
+        }
+
+        _transactions.isExecuted=true;
         emit Execute(_trnxId);
 
     }
 
-    /*
-    Allows an owner to revoke their approval for a transaction identified by its ID.
-    The transaction must exist and must not have been previously executed.
-    The calling owner must have previously approved the transaction.
-    Parameters:
-    - _trnxId: The ID of the transaction to revoke approval for.
-    */
     function revoke(uint256 _trnxId) external
     onlyOwner
     trnxExists(_trnxId)
@@ -173,3 +255,5 @@ contract MultiSignWallet{
        emit Revoke(msg.sender,_trnxId);
     }
 }
+
+// ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db","0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB","0x617F2E2fD72FD9D5503197092aC168c91465E7f2"]
